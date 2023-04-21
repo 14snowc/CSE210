24c8d40b5c337fa72ede0fd04018a0c703779f20
Codie Snow <88008320+14snowc@users.noreply.github.com>
2023-04-20 19:07:37 -0600

    Finished the first 2 assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        string first_name = GetInput("What is your first name? ");
+        string last_name = GetInput("What is your last name? ");
+
+        string output_text = "Your name is " + last_name + ", " + first_name + " " + last_name + ".";
+        Console.WriteLine(output_text);
+    }
+    
+    static string GetInput(string text)
+    {
+        Console.Write(text);
+        string user_text = Console.ReadLine();
+        return user_text;
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade_percent = InputInt("Enter a grade number: ");
+        string letter_grade = GetLetterGrade(grade_percent);
+        Console.WriteLine(letter_grade);
+    }
+
+    static int InputInt(string text)
+    {
+        Console.Write(text);
+        string user_input_string = Console.ReadLine();
+        int user_input = int.Parse(user_input_string);
+        return user_input;
+    }
+
+    static string GetLetterGrade(int grade_percent)
+    {
+        string letter_grade;
+        if (grade_percent >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if(grade_percent >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if(grade_percent >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            return "F";
+        }
+        if (grade_percent % 10 >= 7) 
+        {
+            if (letter_grade != "A")
+            {
+                letter_grade += "+";
+            }
+        }
+        else if (grade_percent % 10 < 3)
+        {
+            letter_grade += "-";
+        }
+        return letter_grade;
     }
 }
