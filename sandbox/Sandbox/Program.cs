4fac2a210c194d3fbd054e892eb8519009b12611
Codie Snow <88008320+14snowc@users.noreply.github.com>
2023-04-27 14:03:22 -0600

    Removed code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,14 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string text = Input("Enter first number: ");
-        Console.WriteLine(text);
+       
     }
 
-    static string Input(string text)
-    {
-        Console.Write(text);
-        string user_input = Console.ReadLine();
-        return user_input;
-    }
 }
