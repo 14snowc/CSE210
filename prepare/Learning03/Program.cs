81b0887120b1bb5d244006d620b9518c9ea40c47
Codie Snow <88008320+14snowc@users.noreply.github.com>
2023-05-16 16:47:05 -0600

    Finished fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        var fraction = new Fraction(1, 3);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+    }
+}
+
+public class Fraction
+{
+
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop()
+    {
+        Console.WriteLine(_top);
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void GetBottom()
+    {
+        Console.WriteLine(_bottom);
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
     }
 }
