b9afe40e88e6481446e3e6abf693b6f9efcb7f9d
Codie Snow <88008320+14snowc@users.noreply.github.com>
2023-05-30 16:28:06 -0600

    Created classes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+    }
+}
+
+class Assignment
+{
+    protected string _studentName;
+    protected string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
+
+class MathAssignment: Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems): base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{GetSummary()} \n{_textbookSection} {_problems}";
+    }
+}
+
+class WritingAssignment: Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title): base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        return $"{GetSummary()} \n{_title}";
     }
 }
