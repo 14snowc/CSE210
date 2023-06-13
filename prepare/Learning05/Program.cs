1bd05c8b19d69ad21b8f5ff0ce87fcda117cb1bf
Codie Snow <88008320+14snowc@users.noreply.github.com>
2023-06-13 14:51:23 -0600

    Finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,93 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        var circle = new Circle("Cyan", 3);
+        var square = new Square("Silver", 5);
+        var rectangle = new Rectangle("Red", 4, 2);
+
+
+        var shapes = new List<Shape> {circle, square, rectangle};
+        foreach (var shape in shapes)
+        {   
+        Console.WriteLine($"{shape.GetColor()} : {shape.GetArea()}");
+        }
+    }
+}
+
+class Shape
+{
+    //Attributes
+    protected string _color;
+
+
+    //Methods
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string newColor)
+    {
+        _color = newColor;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
+
+class Square : Shape
+{
+    //Attributes
+    protected double _size;
+
+    //Methods
+    public Square(string color, double size) : base(color)
+    {
+        _size = size;
+    }
+    public override double GetArea()
+    {
+        return _size * 2;
+    }
+}
+
+class Rectangle : Shape
+{
+    //Attributes
+    protected double _length;
+    protected double _width;
+
+    //Methods
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+
+class Circle : Shape
+{
+    //Attributes
+    protected double _radius;
+
+    //Methods
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,9 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine(5);
+       var hourly = new HourlyEmplyoyee("Elon Musk", 100, 14);
+       var salary = new SalaryEmployee("Robert Oppenheimer", 90000, 14);
+
+       var employees = new List<employyee> { hourly, salary};
+
+       foreach (var employee in employees) {
+            Console.WriteLine($"{employee._name} : {employee.PayPeriodWages()}");
+       }
+
     }
 
    
 }
 
+
+class employyee
+{
+    //Attributes
+    public string _name;
+    protected int _payPeriodLength;
+
+    //Methods
+    public employyee(string name, int payPeriodLength)
+    {
+        _name = name;
+        _payPeriodLength = payPeriodLength;
+    }
+
+    virtual public double PayPeriodWages()
+    {
+        return 0;
+    }
+
+
+}
+
+class HourlyEmplyoyee : employyee
+{
+    //Attributes
+    double _rate;
+
+    //Methods
+
+    public HourlyEmplyoyee(string name, int payPeriodLength, double rate) : base(name, payPeriodLength)
+    {
+        _rate = rate;
+    }
+    public override double PayPeriodWages()
+    {
+        return _rate * 8 * _payPeriodLength;
+    }
+
+}
+
+class SalaryEmployee : employyee
+{
+    //Attributes
+    double _annualRate;
+    
+
+    //Methods
+    public SalaryEmployee(string name, int payPeriodLength, double annualRate) : base(name, payPeriodLength)
+    {
+        _annualRate = annualRate;
+    }
+    public override double PayPeriodWages()
+    {
+        return (_payPeriodLength / 365) * _annualRate;
+    }
+
+}
